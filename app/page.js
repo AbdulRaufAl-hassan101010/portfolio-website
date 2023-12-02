58db34deac455cb57d2ebf9e0d43c1dd4c9a1be0
AbdulRaufAl-hassan101010 <abdulraufalhassan101010@gmail.com>
2023-12-02 21:09:32 +0000

    initial commit

--- app/page.js
+++ app/page.js
@@ -1,113 +1,272 @@
-import Image from 'next/image'
+import Link from "next/link";
+import Image from "next/image";
 
-export default function Home() {
+const page = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <main>
+        <div className="hero">
+          <header className="container mx-auto flex px-10">
+            <section className="w-1/2 flex justify-center flex-col context">
+              <div>
+                {" "}
+                <h1 className="text-6xl font-bold">
+                  Hi, I'm <span className="text-accent">Abdul-Rauf.</span>
+                </h1>
+                <p className="text-2xl mt-10">
+                  I'm a software engineer based in Ghana
+                </p>
+                <Link href="mailto:abdulraufalhassan101010@gmail.com">
+                  <button className="bg-accent hover:bg-secondary text-white font-bold rounded-full p-5 px-10 mt-10">
+                    Contact Me
+                  </button>
+                </Link>
+                <div className="mt-10">
+                  <div>
+                    <small>Connect with me at</small>
+                  </div>
+                  <div className="flex gap-10">
+                    {" "}
+                    <div>
+                      <Link
+                        href="https://github.com/AbdulRaufAl-hassan101010"
+                        target="_blank"
+                      >
+                        <i className="fa-brands fa-github fa-2xl"></i>
+                      </Link>
+                    </div>
+                    <div>
+                      <Link
+                        href="https://www.linkedin.com/in/abdul-rauf-alhassan-b1b4a2225/"
+                        target="_blank"
+                      >
+                        <i className="fa-brands fa-linkedin fa-2xl"></i>
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </section>
+            <section className="w-1/2 bg-secondary flex justify-center flex-col hero-img pt-20 pl-20 ">
+              <Image
+                src="/assets/imgs/hero.png"
+                alt="Hero"
+                className="w-full"
+                width={400}
+                height={200}
+              />
+            </section>
+          </header>
         </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <section className="about container mx-auto px-10 py-20 mb-5">
+          <div className="wrapper">
+            <h2 className="header mb-10 text-accent">About Me.</h2>
+            <p className="mb-20" id="about">
+              I am a software engineer based in Ghana. , I started my
+              programming journey in 2019 as a self taught developer and I have
+              been learning and building ever since. I am currently an{" "}
+              <span className="text-accent">
+                alumnus student of Alx (2022-2023)
+              </span>{" "}
+              . I have a passion for developing software solutions that solve
+              real world problems. I am proficient in JavaScript and I have
+              experience working with React, React Native, NodeJS, ExpressJS,
+              MongoDB, mysql, nextjs, python , flask, no my way around the linux
+              distribution system like (ubuntu). I can create secure rest apis
+              and host and config servers with load balancers. I am currently
+              learning automation with python and machine learning and I am very
+              interested in learning more about machine learning.
+            </p>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+            <div>
+              <h3 className="header text-accent">My Skills.</h3>
+              <li>Javascript, Reactjs, Nextjs</li>
+              <li>Redux And Reduxtoolkit</li>
+              <li>HTLM, CSS And Boostrap</li>
+              <li>React Native</li>
+              <li>Nodejs And Express</li>
+              <li>MongoDb and Mongoose</li>
+              <li>Mysql</li>
+              <li>Python And Flask</li>
+              <li>Linux Distrubution Like (Ubuntu)</li>
+              <li>Automation With Bash And Python</li>
+              <li>Nginx And HAproxy</li>
+              <li>Version Control Such As (Git And Github)</li>
+            </div>
+          </div>
+        </section>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+        <section className="container mx-auto bg-accent py-20 px-10 text-white mb-20">
+          <h3 className="header mb-10">Portfolio Projects</h3>
+          <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-10 mb-10">
+            <article className="">
+              <Image
+                src="/assets/imgs/Clippy.png"
+                alt="Hero"
+                className="mb-10 w-full"
+                width={200}
+                height={200}
+              />
+              <div className="flex gap-5">
+                {" "}
+                <Link
+                  href={"https://clippy-phi.vercel.app/"}
+                  target="_blank"
+                  className="bg-white text-accent p-5 rounded-full"
+                >
+                  Visit Clippy
+                </Link>
+                <Link
+                  href={"https://github.com/AbdulRaufAl-hassan101010/clippy"}
+                  target="_blank"
+                  className="bg-white text-accent p-5 rounded-full"
+                >
+                  Github Repo
+                </Link>
+              </div>
+            </article>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+            <article className="">
+              <Image
+                src="/assets/imgs/Clippy.png"
+                alt="Hero"
+                className="mb-10 w-full"
+                width={400}
+                height={200}
+              />
+              <div className="flex gap-5">
+                <Link
+                  href={"https://bonifytech.tech"}
+                  target="_blank"
+                  className="bg-white text-accent p-5 rounded-full"
+                >
+                  Visit freelance
+                </Link>
+                <Link
+                  href={"https://github.com/AbdulRaufAl-hassan101010/freelance"}
+                  target="_blank"
+                  className="bg-white text-accent p-5 rounded-full"
+                >
+                  Github Repo
+                </Link>
+              </div>
+            </article>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+            <article className="">
+              <Image
+                src="/assets/imgs/Clippy.png"
+                alt="Hero"
+                className="mb-10 w-full"
+                width={200}
+                height={200}
+              />
+              <div className="flex gap-5">
+                <Link
+                  href={
+                    "https://drive.google.com/file/d/1oXLmyQnG1_KieBnRgh1CUfRM6ylM471C/view?usp=sharing"
+                  }
+                  target="_blank"
+                  className="bg-white text-accent p-5 rounded-full"
+                >
+                  Retail App Demo
+                </Link>
+                <Link
+                  href={"https://github.com/AbdulRaufAl-hassan101010/aught"}
+                  target="_blank"
+                  className="bg-white text-accent p-5 rounded-full"
+                >
+                  Github Repo
+                </Link>
+              </div>
+            </article>
+
+            <article className="">
+              <Image
+                src="/assets/imgs/Clippy.png"
+                alt="Hero"
+                className="mb-10 w-full"
+                width={200}
+                height={200}
+              />
+              <div className="flex gap-5">
+                <Link
+                  href={"https://www.youtube.com/watch?v=iov99BA7VeU"}
+                  target="_blank"
+                  className="bg-white text-accent p-5 rounded-full"
+                >
+                  Online store Demo
+                </Link>
+                <Link
+                  href={"https://github.com/AbdulRaufAl-hassan101010/ghantrade"}
+                  target="_blank"
+                  className="bg-white text-accent p-5 rounded-full"
+                >
+                  Github Repo
+                </Link>
+              </div>
+            </article>
+          </div>
+          <div className="header">
+            NB: I couldn't upload some projects because of the hosting fees,
+            please checkout my github for more projects
+          </div>
+        </section>
+
+        <section className="container mx-auto mb-20 py-20 px-10">
+          <h3 className="header mb-20">Certificates</h3>
+          <div className="grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 gap-4">
+            <div className="bg-secondary">
+              <Image
+                src={"/assets/imgs/alx.png"}
+                className="w-full"
+                alt="cert image"
+                width={600}
+                height={200}
+              ></Image>
+            </div>
+          </div>
+        </section>
+      </main>
+      <footer className="bg-secondary">
+        <div className="container mx-auto py-20 px-10">
+          <h4 className="header">Contacts</h4>
+          <div className="grid sm:grid-cols-1 md:grid-cols-2">
+            <address>
+              <ul className="list-none">
+                <li className="mb-3">
+                  <Link href="mailto:abdulraufalhassan101010@gmail.com">
+                    Email: abdulraufalhassan101010@gmail.com
+                  </Link>
+                </li>
+                <li className="mb-3">
+                  <Link href="+233202511342">Phone: +233202511342</Link>
+                </li>
+                <li className="mb-3">
+                  <Link href="+233202511342">City: Greater-Accra</Link>
+                </li>
+                <li className="mb-3">
+                  <Link href="+233202511342">Country: Ghana 🇬🇭</Link>
+                </li>
+              </ul>
+            </address>
+            <div className="flex gap-5 justify-end">
+              <Link href="mailto:abdulraufalhassan101010@gmail.com">
+                <button className="bg-accent hover:bg-secondary text-white font-bold rounded-full p-5 px-10 mt-10">
+                  Resume
+                </button>
+              </Link>
+              <Link href="mailto:abdulraufalhassan101010@gmail.com">
+                <button className="bg-accent hover:bg-secondary text-white font-bold rounded-full p-5 px-10 mt-10">
+                  A Job Will Be Nice 😃
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default page;
---
+++ components/Nav.js
@@ -0,0 +1,39 @@
+import Link from "next/link";
+import { Open_Sans } from "next/font/google";
+
+const open_sans = Open_Sans({
+  weight: ["800"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+  display: "swap",
+});
+
+const Nav = () => {
+  return (
+    <nav className="flex justify-between container mx-auto py-20 items-center px-10">
+      <Link href="/" className={`${open_sans.className} logo`}>
+        Portfolio
+      </Link>
+      <ul className="flex gap-10 items-center">
+        <li className="desktop-link">
+          <Link href="/">Home</Link>
+        </li>
+        <li className="desktop-link">
+          <Link href="#portfolio">Portfolio</Link>
+        </li>
+        <li>
+          <Link href="/about" className="desktop-link">
+            About
+          </Link>
+        </li>
+        <li>
+          <button className="bg-accent hover:bg-secondary text-white font-bold rounded-full p-5 px-10">
+            Resume
+          </button>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
